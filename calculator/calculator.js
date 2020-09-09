0d2ae18952dc9d213957952ef57ea9533cac1c87
turnera <turnermallen30@gmail.com>
2020-09-09 17:54:00 -0500

    calculator excersise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,45 @@
-function add () {
-	
+function add (x, y) {
+	return x + y;
 }
 
-function subtract () {
-	
+function subtract (x, y) {
+	return x - y;
 }
 
-function sum () {
-	
+function sum (x) {
+	let sum = 0;
+	for(let i = 0; i< x.length; i++)
+	{
+		sum += x[i];
+	}
+	return sum;
 }
 
-function multiply () {
-	
+function multiply(x) 
+{
+	let result = 1;
+	for(let i = 0; i < x.length; i++)
+	{
+		result *= x[i];
+	}
+	return result;
 }
 
-function power() {
-	
+function power(x, y) {
+	return Math.pow(x, y);
 }
 
-function factorial() {
+function factorial(num) {
 	
+	if(num < 0)
+		return -1;
+	else if (num == 0)
+		return 1;
+	else
+	{
+		return num * factorial(num - 1);
+	}
+
 }
 
 module.exports = {
